a8b25ade254269b8bb8dac440b686d13cd6f6ea7
lomby <neil.sherwood@gmail.com>
2020-06-21 12:20:32 +0100

    Alias add and alias delete functionality

--- adminapi/users.go
+++ adminapi/users.go
@@ -62,3 +62,56 @@ func GetUser(conn *admin.Service, userKey string) (*admin.User, error) {
 	return result, nil
 
 }
+
+func MakeUserAdmin(conn *admin.Service, userKey string) (string, error) {
+
+	var makeAdmin admin.UserMakeAdmin
+	makeAdmin.Status = true
+
+	err := conn.Users.MakeAdmin(userKey, &makeAdmin).Do()
+
+	if err != nil {
+		return "", err
+	}
+
+	return "Success", nil
+}
+
+func DeleteUser(conn *admin.Service, userKey string) (string, error) {
+
+	err := conn.Users.Delete(userKey).Do()
+
+	if err != nil {
+		return "", err
+	}
+
+	return "Successfully Deleted", nil
+}
+
+func CreateUserAlias(conn *admin.Service, userKey string, alias string) (*admin.Alias, error) {
+
+	newAlias := &admin.Alias{
+		Alias: alias,
+	}
+
+	result, err := conn.Users.Aliases.Insert(userKey, newAlias).Do()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+
+}
+
+func DeleteUserAlias(conn *admin.Service, userKey string, alias string) (string, error) {
+
+	err := conn.Users.Aliases.Delete(userKey, alias).Do()
+
+	if err != nil {
+		return "", err
+	}
+
+	return "Success", nil
+
+}
